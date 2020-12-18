ce6c04864c41582d67851f80416f3112bc771b82
denfad <denfad2003@mail.ru>
2020-12-18 22:31:31 +0300

    Server can update coefficients

--- src/main/java/ru/denfad/kvantorida/services/UpdateDataService.java
+++ src/main/java/ru/denfad/kvantorida/services/UpdateDataService.java
@@ -2,6 +2,9 @@ package ru.denfad.kvantorida.services;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.denfad.kvantorida.models.Person;
@@ -9,14 +12,17 @@ import ru.denfad.kvantorida.models.Place;
 
 import java.util.*;
 
+
+@EnableScheduling
+@Component
 @Transactional
-@Service
 public class UpdateDataService {
 
-    private final int delay = 86400000;
+    private final int delay = 10000;
     private final PlaceService placeService;
     private final PersonService personService;
     private static Map<String, Double> statusMap = new HashMap<>();
+    private static Map<String, Double> statusMapPlace = new HashMap<>();
 
     static {
         statusMap.put("Есть положительный тест", 0.65);
@@ -26,31 +32,36 @@ public class UpdateDataService {
         statusMap.put("Контактировал с больным",0.45);
         statusMap.put("Есть симптомы",0.4);
 
+        statusMapPlace.put("Магазин",0.5);
+        statusMapPlace.put("Аптека",0.75);
+        statusMapPlace.put("Больница",0.9);
+        statusMapPlace.put("Гос. учреждение",0.3);
+        statusMapPlace.put("Общественное место",0.55);
+        statusMapPlace.put("Школа",0.6);
+
     }
 
     @Autowired
     public UpdateDataService(PlaceService placeService, PersonService personService) {
         this.placeService = placeService;
         this.personService = personService;
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                List<Integer> ids = placeService.getPlacesId();
-                for(int id: ids){
-                    Place p = placeService.getPlaceById(id);
-                    double coef = p.getCoefficient();
-                    double mid = 0;
-                    for(Person pers: p.getPersons()){
-                        mid = mid + statusMap.get(pers.getStatus());
-                    }
-                    mid = mid / p.getPersons().size();
-                    coef = coef + mid + Double.valueOf(personService.getIllPersons("Есть положительный тест") )/ 100000;
-                    p.setCoefficient(coef);
-                    placeService.addPlace(p);
-                }
+    }
+
+    @Scheduled(fixedRate = 86400000)
+    public void update(){
+        List<Integer> ids = placeService.getPlacesId();
+        for(int id: ids){
+            Place p = placeService.getPlaceById(id);
+            double coef = statusMapPlace.get(p.getType());
+            double mid = 0;
+            for(Person pers: p.getPersons()){
+                mid = mid + statusMap.get(pers.getStatus());
             }
-        }, delay, delay);
+            mid = mid / p.getPersons().size();
+            coef = coef + mid + Double.valueOf(personService.getIllPersons("Есть положительный тест") )/ 100000;
+            p.setCoefficient(coef);
+            placeService.addPlace(p);
+        }
     }
 }
 
